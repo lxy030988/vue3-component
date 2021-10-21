30cf47e9742671e0de95d94b4ed451e6c871055d
lixiangyu <654762937@qq.com>
2021-10-21 23:50:19 +0800

    打包全局入口文件的 d.ts 和package.json

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -1,7 +1,13 @@
 import { series, parallel } from 'gulp' //series串行打包 parallel并行打包
+import { genTypes } from './gen-types'
 import { run, withTaskName } from './utils'
+import { lxyRoot, outDir } from './utils/paths'
 
 // gulp 不叫打包 做代码转化 vite
+
+const copySourceCode = async () => {
+  await run(`cp ${lxyRoot}/package.json ${outDir}/package.json`)
+}
 
 //1.打包样式 2.打包工具方法 3.打包所有组件,打包每个组件 4.生成一个组件库 5.发布组件
 export default series(
@@ -12,6 +18,7 @@ export default series(
     withTaskName('buildFullComponent', () => run('pnpm run build buildFullComponent')), // 执行build命令时会调用gulp, 我们给gulp传递参数buildFullComponent 那么就会执行导出任务叫 buildFullComponent
     withTaskName('buildComponent', () => run('pnpm run build buildComponent')),
   ),
+  parallel(genTypes, copySourceCode),
 )
 
 export * from './full-component'
